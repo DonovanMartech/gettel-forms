37a2e3bd360b5cf8c5346ec6dbaa8d232f12b425
Austin Lee <alee2117@gmail.com>
2023-02-21 11:19:26 -0500

    linking to Grid on Jotform and displaying the title for that grid

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,48 @@
-var express = require('express');
-var router = express.Router();
-const jotform = require('jotform');
-require('dotenv').config();
-const apiKey = process.env.APIKEY;
+var express = require("express")
+var router = express.Router()
+const jotform = require("jotform")
+require("dotenv").config()
+const apiKey = process.env.APIKEY
 
 jotform.options({
-	debug: true,
-	apiKey: apiKey,
-});
+  debug: true,
+  apiKey: apiKey,
+})
 
-let reports;
+let reports
+let grids
+let grid
+let reportUrl
+let reportTitle
+
 jotform
-	.getReports()
-	.then(function (r) {
-		reports = JSON.stringify(r, null, 4);
-	})
-	.fail(function (e) {
-		/* handle error */
-		console.log(e);
-	});
+  .getReports()
+  .then(function (r) {
+    reports = JSON.stringify(r, null, 4)
+    grids = r.filter((report) => {
+      return report.list_type === "grid"
+    })
+    grid = grids.map((grid) => {
+      reportUrl = grid.url
+	  reportTitle = grid.title
+      console.log(grid)
+      return JSON.stringify(grid, null, 4)
+    })
+  })
+  .fail(function (e) {
+    /* handle error */
+    console.log(e)
+  })
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-	res.render('index', { title: 'Express', reports: reports });
-});
+router.get("/", function (req, res, next) {
+  res.render("index", {
+    title: "Express",
+    reports: reports,
+    grid: grid,
+    reportUrl: reportUrl,
+    reportTitle: reportTitle,
+  })
+})
 
-module.exports = router;
+module.exports = router
