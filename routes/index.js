14bef3baf4bf5951f2f4b6faaa035d1458d883f5
James Cooper <cooper.jm@outlook.com>
2023-02-23 11:29:41 -0500

    experiment

--- routes/index.js
+++ routes/index.js
@@ -1,62 +1,104 @@
-var express = require("express")
-var router = express.Router()
-const jotform = require("jotform")
-require("dotenv").config()
-const apiKey = process.env.APIKEY
+var express = require('express');
+var router = express.Router();
+const jotform = require('jotform');
+require('dotenv').config();
+const apiKey = process.env.APIKEY;
 
 jotform.options({
-  debug: true,
-  apiKey: apiKey,
-})
+	debug: true,
+	apiKey: apiKey,
+});
 
-let reports
-let grid
-let reportUrl
-let reportTitle
-let formUrl
+let reports;
+let grid;
+let reportUrl;
+let reportTitle;
+let formUrl;
+let forms = [];
 
-// jotform
-//   .getReports()
-//   .then(function (r) {
-//     // reports = JSON.stringify(r, null, 4)
-//     let grids = r.filter((report) => {
-//       return report.list_type === "grid"
-//     })
-//     grid = grids.map((grid) => {
-//       reportUrl = grid.url
-//       reportTitle = grid.title
-//       formUrl = grid.form_url
-//       console.log(grid)
-//       return JSON.stringify(grid, null, 4)
-//     })
-//   })
-//   .fail(function (e) {
-//     /* handle error */
-//     console.log(e)
-//   })
-
+//Grabbing all active forms and adding them to forms array, seperated by id and title
 jotform
-  .getFormSubmissions(process.env.FORMID, {
-    filter: {
-      "created_at:gt": "2023-01-25 00:00:00",
-      "created_at:lt": "2023-01-26 00:00:00",
-    },
-  })
-  .then(function (r) {
-    reports = JSON.stringify(r, null, 4)
-    // console.log(reports)
-  })
+	.getForms()
+	.then(function (r) {
+		let formObj = {};
+		activeForms = r.filter((form) => {
+			return form.status === 'ENABLED';
+		});
+		activeForms.map((form) => {
+			formObj = { id: form.id, title: form.title };
+			forms.push(formObj);
+		});
+		// console.log('activeForms: ', activeForms);
+		// console.log('forms: ', forms);
+		return formObj;
+	})
+	.then((formObj) => {
+		jotform
+			.getFormSubmissions(forms[1].id, {
+				filter: {
+					'created_at:gt': '2023-01-26 00:00:00',
+					'created_at:lt': '2023-01-27 00:00:00',
+				},
+			})
+			.then(function (r) {
+				reports = JSON.stringify(r, null, 4);
+			});
+	})
+	.fail(function (e) {
+		console.log(e);
+	});
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", {
-    title: "Express",
-    reports: reports,
-    grid: grid,
-    reportUrl: reportUrl,
-    reportTitle: reportTitle,
-    formUrl: formUrl,
-  })
-})
+router.get('/', function (req, res, next) {
+	res.render('index', {
+		title: 'Express',
+		reports: reports,
+		grid: grid,
+		reportUrl: reportUrl,
+		reportTitle: reportTitle,
+		formUrl: formUrl,
+		forms: JSON.stringify(forms),
+	});
+});
 
-module.exports = router
+router.get('/all', (req, res, next) => {
+	let year = req.query.year;
+	let month = req.query.month;
+	const lastDayOfPreviousMonth =
+		new Date(year, month - 1, -1).toISOString().split('T')[0] + ' 00:00:00';
+	// const lastOfMonth = new Date(year, month, 0);
+	const firstDayOfNextMonth =
+		new Date(year, month, 1).toISOString().split('T')[0] + ' 00:00:00';
+  res.locals.lastDayOfPreviousMonth = lastDayOfPreviousMonth;
+  res.locals.firstDayOfNextMonth = firstDayOfNextMonth;
+
+  // console.log(jSubmissions(lastDayOfPreviousMonth, firstDayOfNextMonth));
+
+	
+  next();
+},(req, res) => {
+  console.log('line 81: ' + res.locals.lastDayOfPreviousMonth);
+  res.render('all', {
+		forms: JSON.stringify(forms),
+		f: res.locals.lastDayOfPreviousMonth,
+		l: res.locals.firstDayOfNextMonth,
+	});
+}
+);
+
+// function jSubmissions(lastMonth, nextMonth) {
+//   let subs = jotform
+// 			.getFormSubmissions(process.env.FORMID, {
+// 				filter: {
+// 					'created_at:gt': lastMonth,
+// 					'created_at:lt': nextMonth,
+// 				},
+// 			})
+// 			.then(function (r) {
+  // 				 return subs = JSON.stringify(r);
+  //         console.log('line 93: ' + subs);
+// 			});
+//   return subs;
+// }
+
+module.exports = router;
