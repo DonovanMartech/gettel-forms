32640a21d502a1a42ab82e87147d5d6f3e5fc102
cooperjm <cooper.jm@outlook.com>
2023-02-24 17:56:35 -0500

    Cleaned up Jotform API pulls

--- routes/index.js
+++ routes/index.js
@@ -9,87 +9,65 @@ jotform.options({
 	apiKey: apiKey,
 });
 
-let reports;
-let grid;
-let reportUrl;
-let reportTitle;
-let formUrl;
 let forms = [];
 let ogForms = [];
 
-//Grabbing all active forms and adding them to forms array, seperated by id and title
-jotform
-	.getForms()
-	.then(function (r) {
-		let formObj = {};
-		activeForms = r.filter((form) => {
-			return form.status === 'ENABLED';
-		});
-		activeForms.map((form) => {
-			ogForms.push(form); //original forms
-			formObj = { id: form.id, title: form.title };
-			forms.push(formObj);
-		});
-		// console.log('activeForms: ', activeForms);
-		// console.log('forms: ', forms);
-		return formObj;
-	})
-	.then((formObj) => {
-		jotform
-			.getFormSubmissions(forms[1].id, {
-				filter: {
-					'created_at:gt': '2023-01-26 00:00:00',
-					'created_at:lt': '2023-01-27 00:00:00',
-				},
-			})
-			.then(function (r) {
-				reports = JSON.stringify(r, null, 4);
-			});
-	})
-	.fail(function (e) {
-		console.log(e);
-	});
+
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
+	ogForms = await getAllForms();
+	console.log(await getAllForms());
 	res.render('index', {
 		title: 'Express',
-		reports: reports,
 		ogForms: ogForms,
-		reportUrl: reportUrl,
-		reportTitle: reportTitle,
-		formUrl: formUrl,
-		forms: JSON.stringify(forms, null, 4),
 	});
 });
 
+// Displaying all submissions from selected month and year
+router.get('/all', async (req, res, next) => {
+	let year = req.query.year;
+	let month = req.query.month;
+	res.locals.lastDayOfPreviousMonth =
+		new Date(year, month - 1, -1).toISOString().split('T')[0] + ' 00:00:00';
+	res.locals.firstDayOfNextMonth =
+		new Date(year, month, 1).toISOString().split('T')[0] + ' 00:00:00';
+	let submissions = await jotGetSubs(res);
+	// console.log(submissions);
+	res.render('all', {
+		forms: JSON.stringify(forms),
+		f: res.locals.lastDayOfPreviousMonth,
+		l: res.locals.firstDayOfNextMonth,
+		subs: submissions,
+	});
+});
 
-
-
-// Get all submissions from selected month
-router.get(
-	'/all',
-	async (req, res, next) => {
-		let year = req.query.year;
-		let month = req.query.month;
-		res.locals.lastDayOfPreviousMonth =
-			new Date(year, month - 1, -1).toISOString().split('T')[0] + ' 00:00:00';
-		res.locals.firstDayOfNextMonth =
-			new Date(year, month, 1).toISOString().split('T')[0] + ' 00:00:00';
-		let submissions = await getSubs(forms, res);
-		// console.log(submissions);
-		res.render('all', {
-			forms: JSON.stringify(forms),
-			f: res.locals.lastDayOfPreviousMonth,
-			l: res.locals.firstDayOfNextMonth,
-			subs: submissions,
+//Grabbing all active forms
+async function getAllForms() {
+	let allForms = await jotform
+		.getForms()
+		.then(function (r) {
+			let activeForms = r.filter((form) => {
+				return form.status === 'ENABLED';
+			});
+			return activeForms;
+		})
+		.fail(function (e) {
+			console.log(e);
 		});
-	}
-);
+	return allForms;
+}
 
-async function jotGetSubs(id, filter) {
+//Grabbing all submissions from selected month and year
+async function jotGetSubs(res) {
+	let filter = {
+		filter: {
+			'created_at:gt': res.locals.lastDayOfPreviousMonth,
+			'created_at:lt': res.locals.firstDayOfNextMonth,
+		},
+	};
 	let submission = await jotform
-		.getFormSubmissions(id, filter)
+		.getSubmissions(filter)
 		.then((response) => {
 			return response;
 		})
@@ -97,25 +75,6 @@ async function jotGetSubs(id, filter) {
 			console.log(e);
 		});
 	return submission;
-}
-
-async function getSubs(forms, res) {
-	let filter = {
-		filter: {
-			'created_at:gt': res.locals.lastDayOfPreviousMonth,
-			'created_at:lt': res.locals.firstDayOfNextMonth,
-		},
-	};
-	// let subs = await jotGetSubs("223544173984160", filter);
-	let subs = await forms.map(async (form) => {
-		let sub = await jotGetSubs(form.id, filter);
-
-		return sub;
-	});
-
-	let subsResult = await Promise.all(subs);
-
-	return subsResult;
 }
 
 module.exports = router;
