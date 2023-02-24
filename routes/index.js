6c7103fb7c79be1ac2f115d64542a7b1b4afd0ab
cooperjm <cooper.jm@outlook.com>
2023-02-24 09:24:38 -0500

    working on getting all submissions from selected date

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,7 @@ let reportUrl;
 let reportTitle;
 let formUrl;
 let forms = [];
+let ogForms = [];
 
 //Grabbing all active forms and adding them to forms array, seperated by id and title
 jotform
@@ -25,6 +26,7 @@ jotform
 			return form.status === 'ENABLED';
 		});
 		activeForms.map((form) => {
+			ogForms.push(form); //original forms
 			formObj = { id: form.id, title: form.title };
 			forms.push(formObj);
 		});
@@ -53,50 +55,67 @@ router.get('/', function (req, res, next) {
 	res.render('index', {
 		title: 'Express',
 		reports: reports,
-		grid: grid,
+		ogForms: ogForms,
 		reportUrl: reportUrl,
 		reportTitle: reportTitle,
 		formUrl: formUrl,
-		forms: JSON.stringify(forms),
+		forms: JSON.stringify(forms, null, 4),
 	});
 });
 
-router.get('/all', (req, res, next) => {
-	let year = req.query.year;
-	let month = req.query.month;
-	res.locals.lastDayOfPreviousMonth =
-		new Date(year, month - 1, -1).toISOString().split('T')[0] + ' 00:00:00';
-	res.locals.firstDayOfNextMonth =
-		new Date(year, month, 1).toISOString().split('T')[0] + ' 00:00:00';
 
-  // console.log(jSubmissions(lastDayOfPreviousMonth, firstDayOfNextMonth));
-  jSubmissions(res.locals.lastDayOfPreviousMonth, res.locals.firstDayOfNextMonth)
-	
-  next();
-},(req, res) => {
-  // console.log('line 81: ' + res.locals.lastDayOfPreviousMonth);
-  
-  res.render('all', {
-		forms: JSON.stringify(forms),
-		f: res.locals.lastDayOfPreviousMonth,
-		l: res.locals.firstDayOfNextMonth,
-	});
-}
+
+
+// Get all submissions from selected month
+router.get(
+	'/all',
+	async (req, res, next) => {
+		let year = req.query.year;
+		let month = req.query.month;
+		res.locals.lastDayOfPreviousMonth =
+			new Date(year, month - 1, -1).toISOString().split('T')[0] + ' 00:00:00';
+		res.locals.firstDayOfNextMonth =
+			new Date(year, month, 1).toISOString().split('T')[0] + ' 00:00:00';
+		let submissions = await getSubs(forms, res);
+		// console.log(submissions);
+		res.render('all', {
+			forms: JSON.stringify(forms),
+			f: res.locals.lastDayOfPreviousMonth,
+			l: res.locals.firstDayOfNextMonth,
+			subs: submissions,
+		});
+	}
 );
 
-function jSubmissions(lastMonth, nextMonth) {
-  let subs;
-  jotform
-			.getFormSubmissions(process.env.FORMID, {
-				filter: {
-					'created_at:gt': lastMonth,
-					'created_at:lt': nextMonth,
-				},
-			})
-			.then(function (r) {
-  				subs = JSON.stringify(r);
-          console.log('line 93: ' + subs);
-			});
+async function jotGetSubs(id, filter) {
+	let submission = await jotform
+		.getFormSubmissions(id, filter)
+		.then((response) => {
+			return response;
+		})
+		.catch((e) => {
+			console.log(e);
+		});
+	return submission;
+}
+
+async function getSubs(forms, res) {
+	let filter = {
+		filter: {
+			'created_at:gt': res.locals.lastDayOfPreviousMonth,
+			'created_at:lt': res.locals.firstDayOfNextMonth,
+		},
+	};
+	// let subs = await jotGetSubs("223544173984160", filter);
+	let subs = await forms.map(async (form) => {
+		let sub = await jotGetSubs(form.id, filter);
+
+		return sub;
+	});
+
+	let subsResult = await Promise.all(subs);
+
+	return subsResult;
 }
 
 module.exports = router;
