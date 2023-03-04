4e52f5b52e9826e657f59befe15a6597062ccdb9
cooperjm <cooper.jm@outlook.com>
2023-03-04 16:01:09 -0500

    Auto select current month and year, auto add years

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,10 +1,28 @@
-console.log(window.location.pathname);
+
+let currentMonth = new Date().getMonth() + 1;
+currentMonth = currentMonth.toString();
+let currentYear = new Date().getFullYear();
+
+// test year
+// currentYear = 2028;
 
 if (window.location.pathname === '/') { 
+  
   const submitButton = document.getElementById('getSubs');
   const btnSpinner = submitButton.children[0];
   const btnText = submitButton.children[1];
+  const monthSelect = document.getElementById('month');
+  const yearSelect = document.getElementById('year');
+  addYears(yearSelect);
+
+  // Set the current month and year in the select elements
+  if (currentMonth < 10) {
+    currentMonth = `0${currentMonth}`;
+  }
+  monthSelect.value = currentMonth;
+  yearSelect.value = currentYear;
   
+  // Remove on load if it is there
   btnSpinner.classList.add('d-none');
 
   submitButton.addEventListener('click', (e) => {
@@ -22,7 +40,23 @@ if (window.location.pathname === '/all') {
   container.classList.remove('container');
   table.style.opacity = 1
   exportBtn.style.transform = 'translateY(0)';
-  console.log(container.classList);
+}
+
+
+function addYears(yearSelect) {
+  const baseYear = 2023;
+  let nowYear = new Date().getFullYear();
+  // test year
+  // nowYear = 2030;
+  let difference = nowYear - baseYear;
+
+  for (let i = 0; i <= difference; i++) {
+    let year = baseYear + i;
+    let option = document.createElement('option');
+    option.value = year;
+    option.innerHTML = year;
+    yearSelect.appendChild(option);
+  }
 }
 
 
